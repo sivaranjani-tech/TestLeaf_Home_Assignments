0e3da6a20de60d0f4c04755452fdd34a90891a2e
Sivaranjani K J <58353146+sivaranjani-tech@users.noreply.github.com>
2026-02-05 08:53:12 +0530

    Add files via upload
    Day1 Home Assignments

---
+++ Day1_HomeAssignment/Primitive_Datatypes.js
@@ -0,0 +1,19 @@
+// userName // String
+let userName="Sivaranjani"
+console.log("Employee's Username:"+" "+userName+" "+ "Typeof: "+typeof(userName))
+
+// organization // String
+let organization="Cognizant"
+console.log("Employee's Organization"+" "+organization+" "+ "Typeof: "+typeof(organization))
+
+// contactNumber // Number
+let contactNumber=9524542650n
+console.log("Employee's Contact Number:"+" "+contactNumber+" "+ "Typeof: "+typeof(contactNumber))
+
+// knowsAutomation // Boolean
+let knowsAutomation=true
+console.log("Did Employee knows Automation?"+" "+knowsAutomation+" "+ "Typeof: "+typeof(knowsAutomation))
+
+// usesPlaywright // Undefined (not assigned)
+let usesPlaywright
+console.log("Did Employee uses playwright?"+" "+usesPlaywright+" "+ "Typeof: "+typeof(usesPlaywright))
